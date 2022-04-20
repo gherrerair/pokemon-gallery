f547831fe1e7fab4dd4117931e89d200e8864f9c
Giovanni Herrera <48238638+gherrerair@users.noreply.github.com>
2022-04-20 11:00:37 -0500

    chore: remove `@supabase/supabase-js`

--- lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICE_ROLE_KEY as string
-);
-
-export default supabaseAdmin;
